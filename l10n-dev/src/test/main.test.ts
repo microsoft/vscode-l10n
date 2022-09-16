b54463b58bc42b552c1108e14842ce4907ae249d
Tyler James Leonhardt <me@tylerleonhardt.com>
2022-09-16 11:23:38 -0400

    override a couple languages (#6)

--- l10n-dev/src/test/main.test.ts
+++ l10n-dev/src/test/main.test.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import assert from 'assert';
-import { getL10nJson, getL10nXlf } from "../main";
+import { getL10nFilesFromXlf, getL10nJson, getL10nXlf } from "../main";
 
 describe('main', () => {
 	context('getL10nJson', () => {
@@ -28,6 +28,55 @@ describe('main', () => {
 			});
 			const xlf = getL10nXlf(map);
 			assert.strictEqual(xlf, '<?xml version="1.0" encoding="utf-8"?>\r\n<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">\r\n  <file original="a" source-language="en" datatype="plaintext"><body>\r\n    <trans-unit id="Hello World">\r\n      <source xml:lang="en">Hello World</source>\r\n    </trans-unit>\r\n  </body></file>\r\n  <file original="b" source-language="en" datatype="plaintext"><body>\r\n    <trans-unit id="Hello Universe">\r\n      <source xml:lang="en">Hello Universe</source>\r\n    </trans-unit>\r\n  </body></file>\r\n</xliff>');
+		});
+	});
+
+	context('getL10nFilesFromXlf', () => {
+		function generateTextXLF(language: string): string {
+			return `
+<?xml version="1.0" encoding="utf-8"?>
+<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
+  <file original="package" source-language="en" datatype="plaintext" target-language="${language}">
+    <body>
+      <trans-unit id="description">
+        <source xml:lang="en">Hello</source>
+        <target state="new">World</target>
+      </trans-unit>
+    </body>
+  </file>
+  <file original="bundle" source-language="en" datatype="plaintext" target-language="${language}">
+    <body>
+      <trans-unit id="description">
+        <source xml:lang="en">Hello</source>
+        <target state="new">World</target>
+      </trans-unit>
+    </body>
+  </file>
+</xliff>
+`
+		}
+
+		it('works', async () => {
+			const details = await getL10nFilesFromXlf(generateTextXLF('de'))
+			assert.strictEqual(details.length, 2);
+			assert.strictEqual(details[0]!.name, 'package');
+			assert.strictEqual(details[0]!.language, 'de');
+			assert.strictEqual(details[0]!.messages['description'], 'World');
+			assert.strictEqual(details[1]!.name, 'bundle');
+			assert.strictEqual(details[1]!.language, 'de');
+			assert.strictEqual(details[1]!.messages['description'], 'World');
+		});
+
+		it('properly changes some of the languages based on VS Code language packs', async () => {
+			let details = await getL10nFilesFromXlf(generateTextXLF('zh-Hans'))
+			assert.strictEqual(details.length, 2);
+			assert.strictEqual(details[0]!.language, 'zh-cn');
+			details = await getL10nFilesFromXlf(generateTextXLF('zh-Hant'))
+			assert.strictEqual(details.length, 2);
+			assert.strictEqual(details[0]!.language, 'zh-tw');
+			details = await getL10nFilesFromXlf(generateTextXLF('pt-BR'))
+			assert.strictEqual(details.length, 2);
+			assert.strictEqual(details[0]!.language, 'pt-br');
 		});
 	});
 });
