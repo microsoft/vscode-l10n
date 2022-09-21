1f634bb984540d8f3468e2ba643b9d401c8f46c4
Tyler James Leonhardt <me@tylerleonhardt.com>
2022-09-21 14:48:49 -0400

    escape double quotes if in id (#8)

--- l10n-dev/src/xlf/test/xlf.test.ts
+++ l10n-dev/src/xlf/test/xlf.test.ts
@@ -13,4 +13,33 @@ describe('XLF', () => {
         const result = xlf.toString();
         assert.strictEqual(result, '<?xml version="1.0" encoding="utf-8"?>\r\n<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">\r\n  <file original="bundle" source-language="en" datatype="plaintext"><body>\r\n    <trans-unit id="Hello">\r\n      <source xml:lang="en">Hello</source>\r\n    </trans-unit>\r\n  </body></file>\r\n</xliff>');
     });
+
+    it('escapes double quotes', () => {
+        const xlf = new XLF();
+        xlf.addFile('bundle', { '"': 'Hello' });
+        const result = xlf.toString();
+        assert.strictEqual(result, '<?xml version="1.0" encoding="utf-8"?>\r\n<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">\r\n  <file original="bundle" source-language="en" datatype="plaintext"><body>\r\n    <trans-unit id="&quot;">\r\n      <source xml:lang="en">Hello</source>\r\n    </trans-unit>\r\n  </body></file>\r\n</xliff>');
+    });
+
+    it('parses double quotes correctly', async () => {
+        const result = await XLF.parse(`
+<?xml version="1.0" encoding="utf-8"?>
+<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
+<file original="package" source-language="en" datatype="plaintext" target-language="de">
+<body>
+    <trans-unit id="&quot;">
+        <source xml:lang="en">Hello</source>
+        <target state="translated">World</target>
+    </trans-unit>
+</body>
+</file>
+</xliff>`);
+
+        assert.ok(result);
+        assert.strictEqual(result.length, 1);
+        assert.strictEqual(result[0]!.language, 'de');
+        assert.strictEqual(result[0]!.name, 'package');
+        assert.ok(result[0]!.messages['"']);
+        assert.strictEqual(result[0]!.messages['"'], 'World');
+    });
 });
