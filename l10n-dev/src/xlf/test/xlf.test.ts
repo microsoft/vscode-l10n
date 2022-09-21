e9d9ca7be6cf73dcbafa03a1f3304aef99225f4f
Tyler James Leonhardt <me@tylerleonhardt.com>
2022-09-21 15:54:01 -0400

    mark regex as global (#9)

--- l10n-dev/src/xlf/test/xlf.test.ts
+++ l10n-dev/src/xlf/test/xlf.test.ts
@@ -16,9 +16,9 @@ describe('XLF', () => {
 
     it('escapes double quotes', () => {
         const xlf = new XLF();
-        xlf.addFile('bundle', { '"': 'Hello' });
+        xlf.addFile('bundle', { '""': 'Hello' });
         const result = xlf.toString();
-        assert.strictEqual(result, '<?xml version="1.0" encoding="utf-8"?>\r\n<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">\r\n  <file original="bundle" source-language="en" datatype="plaintext"><body>\r\n    <trans-unit id="&quot;">\r\n      <source xml:lang="en">Hello</source>\r\n    </trans-unit>\r\n  </body></file>\r\n</xliff>');
+        assert.strictEqual(result, '<?xml version="1.0" encoding="utf-8"?>\r\n<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">\r\n  <file original="bundle" source-language="en" datatype="plaintext"><body>\r\n    <trans-unit id="&quot;&quot;">\r\n      <source xml:lang="en">Hello</source>\r\n    </trans-unit>\r\n  </body></file>\r\n</xliff>');
     });
 
     it('parses double quotes correctly', async () => {
@@ -27,7 +27,7 @@ describe('XLF', () => {
 <xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
 <file original="package" source-language="en" datatype="plaintext" target-language="de">
 <body>
-    <trans-unit id="&quot;">
+    <trans-unit id="&quot;&quot;">
         <source xml:lang="en">Hello</source>
         <target state="translated">World</target>
     </trans-unit>
@@ -39,7 +39,7 @@ describe('XLF', () => {
         assert.strictEqual(result.length, 1);
         assert.strictEqual(result[0]!.language, 'de');
         assert.strictEqual(result[0]!.name, 'package');
-        assert.ok(result[0]!.messages['"']);
-        assert.strictEqual(result[0]!.messages['"'], 'World');
+        assert.ok(result[0]!.messages['""']);
+        assert.strictEqual(result[0]!.messages['""'], 'World');
     });
 });
