f44114a2869a1d2827423b15c6ed54d5037bef19
Tyler Leonhardt <me@tylerleonhardt.com>
2023-05-06 22:31:54 -0700

    Add debug and verbose logging
    Now you can use `-d` for debug logging which is super chatty... and `-v` for verbose which is slightly more detailed than normal.

---
+++ l10n-dev/src/logger.ts
@@ -0,0 +1,34 @@
+
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import debug from 'debug';
+
+export const enum LogLevel {
+    Verbose = 'VERBOSE',
+    Debug = 'DEBUG',
+    Warning = 'WARNING'
+}
+
+debug.enable(LogLevel.Warning);
+
+export const logger = {
+    verbose: debug(LogLevel.Verbose),
+    debug: debug(LogLevel.Debug),
+    warning: debug(LogLevel.Warning),
+    log: console.log,
+    error: console.error,
+    setLogLevel(level: LogLevel) {
+        switch (level) {
+            case LogLevel.Debug:
+                debug.enable(LogLevel.Debug);
+            case LogLevel.Verbose:
+                debug.enable(LogLevel.Verbose);
+            default:
+                break;
+        }
+        debug.enable(level);
+    }
+};
